3c1a91030547a34a6d20adc2a97b46cb3e9afd02
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-02-19 15:11:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MoodAnalyzerProgram
+{
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string message = "I am so happy today!";
+            string mood = moodAnalyser.AnalyseMood(message);
+            Console.WriteLine(mood);
+
+            message = "I feel really sad today";
+            mood = moodAnalyser.AnalyseMood(message);
+            Console.WriteLine(mood);
+        }
+            
+    }
+}
